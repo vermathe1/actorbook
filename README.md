f5ea6f5872ebd56451d3489db6831f30a349d520
Pramod Verma <pramod.verma@tesco.com>
2022-03-14 18:45:34 +0530

    configure webpack 5 and run the initial build

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# actorbook
+# Project Title
+
+Episode Search Book
+
+## Description
+
+Simple Episode Searching Application Based on Characters.
+
+### Installing
+
+Go to root project where we have package.json located
+Run the below commands
+
+1. npm install
+2. npm run build
+3. npm start
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    open: true,
+  },
+}
